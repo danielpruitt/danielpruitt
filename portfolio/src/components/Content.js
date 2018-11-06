5f0ac53ed7341b16c607abacb037b30ed23e3013
danielpruitt <35474097+danielpruitt@users.noreply.github.com>
2018-11-06 11:57:09 -0500

    Updates

--- portfolio/src/components/Content.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Navbar from "./Navbar";
-
-const Content = () => (
-    <Navbar />
-
-);
-
-export default Content;
--- portfolio/src/components/Grid/Row.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Row.css";
-
-export const Row = ({ children }) =>
-    <div className="row">
-        {children}
-    </div>;
-
-export default Row;
